7d7792540fa48aa69fea89b79c98b3510d7f00e9
Sarah Colbert <47012384+sarahcolbert@users.noreply.github.com>
2021-12-22 11:26:38 -0500

    Update actual_code.bash

--- abcd_test/actual_code.bash
+++ abcd_test/actual_code.bash
@@ -26,6 +26,8 @@ export data_dir="/scratch/aalab/suri/data/cleaned_data/" ## will make this $1
 echo "reading data from ${data_dir}"
 ## get name of plink files
 export input_prefix="abcd_eur_sibs" ## will make this $2
+## get location for covariate file
+export covar_file="/scratch/aalab/suri/data/raw_data/abcd_covariates_eur.txt"
 
 ############################
 ##### QC AND CALL ROHS #####
@@ -37,27 +39,10 @@ plink --bfile ${data_dir}${input_prefix} --maf 0.05 --geno 0.03 --mind 0.03 --ma
 ## use plink to identify continuous ROH SNPs
 plink --bfile ${processed_dir}/${input_prefix}_filtered --homozyg-window-snp 50 --homozyg-snp 50  --homozyg-kb 1500  --homozyg-gap 1000  --homozyg-density 50 --homozyg-window-missing 5 --homozyg-window-het 1 --out ${processed_dir}/${input_prefix}_roh
 
-############################
+######################################
+##### CALCULATE FROH WITHIN SIBS #####
+######################################
+
 ## use R to calculate froh
-
-## load packages and data
-library(tidyverse)
-roh_data <- read.table(paste(Sys.getenv("processed_dir"),Sys.getenv("input_prefix"),"_roh.hom.indiv", sep=""), header = TRUE)
-
-## calc froh
-roh_data$froh <- roh_data$KB/(2.77*10^6)
-
-## filter to necessary columns
-froh_data <- roh_data %>% select(IID, NSEG, KB, froh)
-
-## calculate minimum, maximum and mean for NSEG, KB and froh
-min_vals <- as.data.frame(apply(froh_data[,2:4], 2, FUN = min, na.rm = TRUE))
-colnames(min_vals)[1] <- "min"
-mean_vals <- as.data.frame(apply(froh_data[,2:4], 2, FUN = mean, na.rm = TRUE))
-colnames(mean_vals)[1] <- "mean"
-max_vals <- as.data.frame(apply(froh_data[,2:4], 2, FUN = max, na.rm = TRUE))
-colnames(max_vals)[1] <- "max" 
-##########
-########## ADD MEDIAN
-##########
+Rscript calc_froh.R
 
