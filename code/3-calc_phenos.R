9e5d92d5963f4680a2dc49b3c573b599cd4c150b
Sarah Colbert <47012384+sarahcolbert@users.noreply.github.com>
2022-01-03 13:22:28 -0500

    added code for looping through phenotypes

--- code/3-calc_phenos.R
+++ code/3-calc_phenos.R
@@ -15,11 +15,23 @@ fam_data <- fam_file %>% select(FID, IID)
 phenotype_data <- merge(fam_data, pheno2, by ="IID")
 
 ## calculate value of phenotype to family mean
-## make empty column to hold results
-phenotype_data$col2 <- NA
-## use for loop to get value for each individual
-for(i in 1:length(phenotype_data$IID){
+## use for loop to go through each of the phenotype columns
+num_phenos <- ncol(phenotype_data)
+for(j in 3:num_phenos){
+  ## make empty column to hold results
+  phenotype_data$newcol <- NA
+  newcolnum <- which(colnames(phenotype_data)=="newcol")
+  ## use for loop to get value for each individual
+for(i in 1:length(phenotype_data$IID)){
   spec_FID <- phenotype_data$FID[i]
   fam_vals <- phenotype_data %>% filter(FID==spec_FID)
-  phenotype_data$col2[i] <- phenotype_data$school_2_y[i]-mean(fam_vals$school_2_y)
+  phenotype_data$newcol[i] <- phenotype_data[i,j]-mean(fam_vals[,j])
 }
+colnames(phenotype_data)[newcolnum] <- paste(colnames(phenotype_data)[j],"_sibs",sep="")
+}
+ 
+## write results to table so that easy to import for regression analysis
+sib_pheno_results <- phenotype_data
+sib_pheno_results[,3:num_phenos] <- NULL
+write.table(sib_pheno_results, paste(Sys.getenv("processed_dir"),"within_sibs_pheno_data.txt", sep=""), quote = FALSE, row.names=FALSE)
+
