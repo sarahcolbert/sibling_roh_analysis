4cdb39adccda9cd1366eac398e9b86468c3eb42f
Sarah Colbert <47012384+sarahcolbert@users.noreply.github.com>
2022-01-20 17:36:46 -0500

    updated to include singletons

--- code/2-calc_froh.R
+++ code/2-calc_froh.R
@@ -41,8 +41,12 @@ for(i in 1:length(froh_data$IID)){
 
 message("done calculating Froh")
 
+## combine info for all individuals
+id_sibs_froh <- froh_data %>% select(IID, froh_sibs)
+all_froh_data <- merge(id_sibs_froh, roh_data, by = "IID", all = TRUE)
+
 ## save table with froh values just incase
-write.table(froh_data, paste(Sys.getenv("processed_dir"),"within_sibs_froh_data.txt", sep=""), row.names=FALSE, quote = FALSE)
+write.table(all_froh_data, paste(Sys.getenv("processed_dir"),"all_froh_data.txt", sep=""), row.names=FALSE, quote = FALSE)
 message(paste("wrote Froh estimates to ",Sys.getenv("processed_dir"),"within_sibs_froh_data.txt", sep=""))
 
 ################################
@@ -51,32 +55,32 @@ message(paste("wrote Froh estimates to ",Sys.getenv("processed_dir"),"within_sib
 
 ## calculate samples minimum, maximum, mean and median for NSEG and froh
 message("calculating sample's minimum, maximum, mean and median for NSEG and Froh")
-min_vals <- as.data.frame(apply(select(froh_data, c("NSEG","froh")), 2, FUN = min, na.rm = TRUE))
+min_vals <- as.data.frame(apply(select(all_froh_data, c("NSEG","froh")), 2, FUN = min, na.rm = TRUE))
 colnames(min_vals)[1] <- "min"
-mean_vals <- as.data.frame(apply(select(froh_data, c("NSEG","froh")), 2, FUN = mean, na.rm = TRUE))
+mean_vals <- as.data.frame(apply(select(all_froh_data, c("NSEG","froh")), 2, FUN = mean, na.rm = TRUE))
 colnames(mean_vals)[1] <- "mean"
-max_vals <- as.data.frame(apply(select(froh_data, c("NSEG","froh")), 2, FUN = max, na.rm = TRUE))
+max_vals <- as.data.frame(apply(select(all_froh_data, c("NSEG","froh")), 2, FUN = max, na.rm = TRUE))
 colnames(max_vals)[1] <- "max" 
-sd_vals <- as.data.frame(apply(select(froh_data, c("NSEG","froh")), 2, FUN = sd, na.rm = TRUE))
+sd_vals <- as.data.frame(apply(select(all_froh_data, c("NSEG","froh")), 2, FUN = sd, na.rm = TRUE))
 colnames(sd_vals)[1] <- "median" 
 message("done calculating sample's minimum, maximum, mean and standard deviation for NSEG and Froh")
 
 ## put descriptive roh stats in data table and save as file (this file will be returned to us)
 message ("writing table with Froh stats")
 descript_roh_stats <- cbind(min_vals, max_vals, mean_vals, sd_vals)
-descript_roh_stats$n_indivs <- rep(length(froh_data$IID), length(descript_roh_stats$min))
+descript_roh_stats$n_indivs <- rep(length(all_froh_data$IID), length(descript_roh_stats$min))
 descript_roh_stats$n_sibgroups <- rep(length(unique(froh_data$FID)), length(descript_roh_stats$min))
 ## check how many individuals have froh = 0
-zero_inds <- sum(froh_data$froh==0)
+zero_inds <- sum(all_froh_data$froh==0)
 descript_roh_stats$n_zero_inds <- rep(zero_inds, length(descript_roh_stats$min))
 ## check how many individuals are offspring of various relative relationships
-half_sib_inds <- sum(froh_data$froh > 0.125)
+half_sib_inds <- sum(all_froh_data$froh > 0.125)
 descript_roh_stats$n_half_sib_inds <- rep(half_sib_inds, length(descript_roh_stats$min))
-first_cous_inds <- sum(froh_data$froh > 0.0625)
+first_cous_inds <- sum(all_froh_data$froh > 0.0625)
 descript_roh_stats$n_first_cous_inds <- rep(first_cous_inds, length(descript_roh_stats$min))
-half_cous_inds <- sum(froh_data$froh > 0.03125)
+half_cous_inds <- sum(all_froh_data$froh > 0.03125)
 descript_roh_stats$n_half_cous_inds <- rep(half_cous_inds, length(descript_roh_stats$min))
-sec_cous_inds <- sum(froh_data$froh > 0.0156)
+sec_cous_inds <- sum(all_froh_data$froh > 0.0156)
 descript_roh_stats$n_sec_cous_inds <- rep(sec_cous_inds, length(descript_roh_stats$min))
 
 write.csv(descript_roh_stats, paste(Sys.getenv("output_dir"),Sys.getenv("output_name"),"_descriptive_roh_stats.csv", sep=""), row.names = TRUE)
