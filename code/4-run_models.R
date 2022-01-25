d8b5b2825811ab6cdb63cf2e5736fd2e5f6d0858
Sarah Colbert <47012384+sarahcolbert@users.noreply.github.com>
2022-01-25 14:54:46 -0500

    Update 4-run_models.R

--- code/4-run_models.R
+++ code/4-run_models.R
@@ -10,10 +10,10 @@ library(lmerTest)
 froh_data <- read.table(paste(Sys.getenv("processed_dir"),"within_sibs_froh_data.txt", sep=""), header = TRUE)
 
 ## scale froh 
-scaled_froh <- froh_data %>% mutate_at(froh, scale)
+scaled_froh <- froh_data %>% mutate_at(c("froh"), scale)
 
 ## load phenotype data
-within_phenotype_data <- read.table(paste(Sys.getenv("processed_dir"),"within_sibs_pheno_data.txt", header = TRUE)
+within_phenotype_data <- read.table(paste(Sys.getenv("processed_dir"),"within_sibs_pheno_data.txt", sep=""), header = TRUE)
 btwn_phenotype_data <- read.table(paste(Sys.getenv("processed_dir"),"btwn_sibs_pheno_data.txt", sep=""), header = TRUE)
 
 ## load covariate data
@@ -42,21 +42,32 @@ btwn_data1 <- btwn_data
 
 ## calculate number of phenotypes
 num_phenos_btwn <- ncol(btwn_data1)
-
-for(k in X:num_phenos_btwn){
-## copy df
-btwn_data1 <- btwn_data
-####### Step 1: regress phenotypes on covariates to get residuals
-  pheno_model <- lmer(paste(colnames(btwn_data1)[k]) ~ age + sex + PC1 + PC2 + Pc3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10, data = btwn_data1)
-####### Step 2: pull residuals from the model
-  pheno_resids <- add_residuals(btwn_data1, pheno_model, var = "resids")
-####### Step 3: regress residuals on froh
-    resids_model <- lmer(resids ~ froh, data = pheno_resids)
-####### Step 4: save results
-summary(resids_model)
-}
-
+                                 
+################################
+### this is for continuous variables                                    
                                     
+for(k in X:num_phenos_btwn){
+  ## copy df
+  btwn_data1 <- btwn_data
+  ## determine if binary or continuous phenotype
+    if(all(btwn_data1[k] %in% c(0,1))){
+      ## binary phenotype calculations
+          }else{
+              ## continuous phenotypes calculations
+              ## code binary variable  
+              btwn_data1)[k]
+              ####### Step 1: regress phenotypes on covariates to get residuals (Clark et al. equation 11)
+              pheno_model <- lmer(paste(colnames(btwn_data1)[k]) ~ age + sex + PC1 + PC2 + Pc3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10, data = btwn_data1)
+              ####### Step 2: pull residuals from the model
+              pheno_resids <- add_residuals(btwn_data1, pheno_model, var = "resids")
+              ####### Step 3: regress residuals on froh (Clark et al. equation 13a)
+              resids_model <- lmer(resids ~ froh, data = pheno_resids)
+              ####### Step 4: save results
+              summary(resids_model)
+               }
+          }
+
+                                                        
                                     
 ###################################
 ######## WITHIN ANALYSIS ##########
