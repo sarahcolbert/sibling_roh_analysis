0210f67f0a1a8e45e1e861739400d94bddb4b7fe
Sarah Colbert <sarah.colbert@wustl.edu>
2022-03-22 16:06:43 -0500

    glmer -> glm

--- code/3-calc_phenos.R
+++ code/3-calc_phenos.R
@@ -121,7 +121,7 @@ for(m in 15:ncol(within_data1)){
     message(paste("Reading",colnames(within_data3)[m], "as a binary trait and will use logistic regression calculate trait residuals", sep=" "))
     ## binary phenotypes calculations
     ####### Step 1: regress phenotypes on covariates to get residuals (Clark et al. equation 11)
-    resids_modelb <- glmer(formula(paste(colnames(within_data3)[m],'~ age + sex + PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10')), data = within_data3, family = binomial(link='logit'), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+    resids_modelb <- glm(formula(paste(colnames(within_data3)[m],'~ age + sex + PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10')), data = within_data3, family = binomial(link='logit'), control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
     ####### Step 2: pull residuals from the model
     pheno_residsb <- within_data3
     pheno_residsb$newcol <- resids_modelb$resid
