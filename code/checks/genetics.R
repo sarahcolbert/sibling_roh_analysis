28e36061892076099cf47a7e3d7d951096f69e2e
Sarah Colbert <sarah.colbert@wustl.edu>
2022-03-25 18:55:32 -0500

    started to add checks

---
+++ code/checks/genetics.R
@@ -0,0 +1,6 @@
+## come up with what we need to check...
+  ## number of SNPs
+    ## > 250k and < X amount (what would we expect to see if we want to send warning to check that data is not imputed)
+  ## duplicate SNPs
+  ## check fam file for duplicate individuals
+  ## since these plink files are only used as input for plink we shouldn't really have to check much and they should be compatible?
