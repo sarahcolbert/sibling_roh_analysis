02ee297c497f92204c1c0b8a1c8ac96217429297
Sarah Colbert <47012384+sarahcolbert@users.noreply.github.com>
2022-03-31 15:59:20 -0600

    Update 1-qc_and_call.bash

--- code/1-qc_and_call.bash
+++ code/1-qc_and_call.bash
@@ -1,7 +1,7 @@
 source ./config
 
 ## use plink to exclude SNPs with >3% missingess or MAF < 5% AND exclude individuals with >3% missing data
-${plink} --bfile ${data_dir}${input_prefix} --maf 0.05 --geno 0.03 --mind 0.03 --make-bed --out ${processed_dir}${input_prefix}_filtered
+${plink} --bfile ${data_dir}${input_prefix} --chr 1-22 --maf 0.05 --geno 0.03 --mind 0.03 --make-bed --out ${processed_dir}${input_prefix}_filtered
 
 ## use plink to identify continuous ROH SNPs
 ${plink} --bfile ${processed_dir}${input_prefix}_filtered --homozyg-window-snp 50 --homozyg-snp 50  --homozyg-kb 1500  --homozyg-gap 1000  --homozyg-density 50 --homozyg-window-missing 5 --homozyg-window-het 1 --out ${processed_dir}${input_prefix}_roh
